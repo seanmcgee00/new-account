84cf63019900a76ec1e0ed617f1c7ad9f3feb8f4
seanmcgee00 <D00121058@sutdent.dkit.ie>
2018-02-21 15:04:06 +0000

    database creation and import of data

---
+++ scr/main/resources/META-INF/import.sql
@@ -0,0 +1,3 @@
+insert into Account (firstName,secondName,accountNumber)values('Joe','Bloggs','1234c');
+insert into Account (firstName,secondName,accountNumber)values('Jane','Doe','1544c');
+insert into Account (firstName,secondName,accountNumber)values('John','Grodon','1345v');
